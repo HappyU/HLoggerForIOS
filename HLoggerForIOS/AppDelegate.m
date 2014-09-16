3609b579d2c352a3e044e74b65b2328674b382f9
wxy <99700415@qq.com>
2014-09-16 15:56:03 +0800

    sdsd

--- HLoggerForIOS/AppDelegate.m
+++ HLoggerForIOS/AppDelegate.m
@@ -14,6 +14,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    /**
+     *  ssdsd
+     */
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
