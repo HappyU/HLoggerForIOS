6e08830a30be69e79adbaf4dff238edc03d88c60
HappyU <343686681@qq.com>
2014-09-16 15:51:50 +0800

    日志组件
    通过点线面的方式，自定义日志的记录和输出

---
+++ HLoggerForIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HLoggerForIOS
+//
+//  Created by 刘博 on 14-9-16.
+//  Copyright (c) 2014年 hirisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HLoggerForIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HLoggerForIOS
+//
+//  Created by 刘博 on 14-9-16.
+//  Copyright (c) 2014年 hirisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
