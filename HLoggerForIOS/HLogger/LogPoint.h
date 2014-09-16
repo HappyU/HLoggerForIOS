e112876f65ec7b4d1ff43cb815956d3276afeb96
HappyU <343686681@qq.com>
2014-09-16 15:59:11 +0800

    添加文件
    日志记录点文件

---
+++ HLoggerForIOS/HLogger/LogPoint.h
@@ -0,0 +1,13 @@
+//
+//  LogPoint.h
+//  HLoggerForIOS
+//
+//  Created by 刘博 on 14-9-16.
+//  Copyright (c) 2014年 hirisun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogPoint : NSObject
+
+@end
