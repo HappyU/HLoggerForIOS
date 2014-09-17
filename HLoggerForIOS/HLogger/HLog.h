6b82c0dea1130077abf5329b307e7d97d26fbe40
HappyU <343686681@qq.com>
2014-09-17 14:49:37 +0800

    添加文件

---
+++ HLoggerForIOS/HLogger/HLog.h
@@ -0,0 +1,13 @@
+//
+//  HLog.h
+//  HLoggerForIOS
+//
+//  Created by 刘博 on 14-9-17.
+//  Copyright (c) 2014年 hirisun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HLog : NSObject
+
+@end
---
+++ HLoggerForIOS/HLogger/HLog.m
@@ -0,0 +1,13 @@
+//
+//  HLog.m
+//  HLoggerForIOS
+//
+//  Created by 刘博 on 14-9-17.
+//  Copyright (c) 2014年 hirisun. All rights reserved.
+//
+
+#import "HLog.h"
+
+@implementation HLog
+
+@end
