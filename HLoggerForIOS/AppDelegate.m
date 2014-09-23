41c6bb5c88d8a68cf27f1cc2f0386eed2e16717c
wxy <99700415@qq.com>
2014-09-23 10:24:04 +0800

    更新

--- HLoggerForIOS/AppDelegate.m
+++ HLoggerForIOS/AppDelegate.m
@@ -8,15 +8,32 @@
 
 #import "AppDelegate.h"
 #import "Logger.h"
+
+#include <libkern/OSAtomic.h>
+#include <execinfo.h>
+
+// 系统信号截获处理方法
+void signalHandler(int signal);
+// 异常截获处理方法
+void exceptionHandler(NSException *exception);
+const int32_t _uncaughtExceptionMaximum = 10;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-
+    
+    installExceptionHandler();
+    
+    [self redirectNSLogToDocumentFolder];
     
     HLOGINFO(@"3333%@,%@",@"ddddsf",@"mmmmm");
+    
+    
+    NSArray *arr = [NSArray arrayWithObject:@"11"];
+    
+    NSString *aa = [arr objectAtIndex:10];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
@@ -49,5 +66,104 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+void signalHandler(int signal)
+{
+    
+    volatile int32_t _uncaughtExceptionCount = 0;
+    int32_t exceptionCount = OSAtomicIncrement32(&_uncaughtExceptionCount);
+    if (exceptionCount > _uncaughtExceptionMaximum) // 如果太多不用处理
+    {
+        return;
+    }
+    
+    // 获取信息
+    NSMutableDictionary *userInfo =
+    [NSMutableDictionary dictionaryWithObject:[NSNumber numberWithInt:signal] forKey:@"UncaughtExceptionHandlerSignalKey"];
+    
+    NSArray *callStack = [AppDelegate backtrace];
+    [userInfo  setObject:callStack  forKey:@"SingalExceptionHandlerAddressesKey"];
+    
+    // 现在就可以保存信息到本地［］
+}
+
+void exceptionHandler(NSException *exception)
+{
+    /*
+     volatile int32_t _uncaughtExceptionCount = 0;
+     int32_t exceptionCount = OSAtomicIncrement32(&_uncaughtExceptionCount);
+     if (exceptionCount > _uncaughtExceptionMaximum) // 如果太多不用处理
+     {
+     return;
+     }
+     
+     NSArray *callStack = [UncaughtExceptionHandler backtrace];
+     NSMutableDictionary *userInfo =[NSMutableDictionary dictionaryWithDictionary:[exception userInfo]];
+     [userInfo setObject:callStack forKey:@"ExceptionHandlerAddressesKey"];
+     
+     // 现在就可以保存信息到本地［］
+     */
+    
+    NSArray *callStackSymbols = [exception callStackSymbols];
+    NSString *callStackSymbolStr = [callStackSymbols componentsJoinedByString:@"\n"];
+    NSString *reason = [exception reason];
+    NSString *name = [exception name];
+    
+//    DDLogError(@"异常 >>");
+//    DDLogError(@"异常名称：%@",name);
+//    DDLogError(@"异常原因：%@",reason);
+//    DDLogError(@"堆栈标志：%@",callStackSymbolStr);
+    
+    NSString *string = [NSString stringWithFormat:@"%@+%@+%@",name,reason,callStackSymbols];
+    
+    
+    HLOGERR(string);
+    
+}
+
+//获取调用堆栈
++(NSArray *)backtrace
+{
+    void* callstack[128];
+    int frames = backtrace(callstack, 128);
+    char **strs = backtrace_symbols(callstack,frames);
+    
+    NSMutableArray *backtrace = [NSMutableArray arrayWithCapacity:frames];
+    for (int i=0;i<frames;i++)
+    {
+        [backtrace addObject:[NSString stringWithUTF8String:strs[i]]];
+    }
+    free(strs);
+    
+    return backtrace;
+}
+
+// 注册崩溃拦截
+void installExceptionHandler()
+{
+    NSSetUncaughtExceptionHandler(&exceptionHandler);
+    signal(SIGHUP, signalHandler);
+    signal(SIGINT, signalHandler);
+    signal(SIGQUIT, signalHandler);
+    
+    signal(SIGABRT, signalHandler);
+    signal(SIGILL, signalHandler);
+    signal(SIGSEGV, signalHandler);
+    signal(SIGFPE, signalHandler);
+    signal(SIGBUS, signalHandler);
+    signal(SIGPIPE, signalHandler);
+}
+
+- (void)redirectNSLogToDocumentFolder
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *fileName = [NSString stringWithFormat:@"%@.log",[NSDate date]];
+    NSString *logFilePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+    freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding],"a+",stderr);
+    
+}
+
+
 
 @end
